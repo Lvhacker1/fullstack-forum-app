5cadc11c8feb047446a0e694c054d81df287aea4
Lvhacker1 <lavasha2@hotmail.com>
2025-11-14 13:37:59 +0100

    small changes on data

--- lib/data/loginText.ts
+++ lib/data/loginText.ts
@@ -2,9 +2,9 @@ import type { LoginPageText } from '@/lib/types/loginText'
 import { ROUTES } from '@/lib/constants/routes'
 
 export const loginText: LoginPageText = {
-  title: 'Login',
-  emailLabel: 'Email',
-  passwordLabel: 'Password',
+  title: 'LOGIN',
+  emailLabel: 'Email:',
+  passwordLabel: 'Password:',
   submitButton: 'Login',
   loading: 'Loading...',
   alternativeText: "Don't have an account?",
