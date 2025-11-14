7fa979d9a4319d0518b7a662d79550a3033a9d61
Lvhacker1 <lavasha2@hotmail.com>
2025-11-14 12:28:25 +0100

    add types

---
+++ lib/types/loginText.ts
@@ -0,0 +1,10 @@
+export interface LoginPageText {
+  title: string
+  emailLabel: string
+  passwordLabel: string
+  submitButton: string
+  loading: string
+  alternativeText: string
+  alternativeLink: string
+  alternativeLinkText: string
+}
