6157270e69a47743afb61767096a53a75e28ce9b
Lvhacker1 <lavasha2@hotmail.com>
2025-11-14 13:38:40 +0100

    add placeholder types

--- lib/types/loginText.ts
+++ lib/types/loginText.ts
@@ -1,7 +1,9 @@
 export interface LoginPageText {
   title: string
   emailLabel: string
+  emailPlaceholder:string
   passwordLabel: string
+  passwordPlaceholder:string
   submitButton: string
   loading: string
   alternativeText: string
