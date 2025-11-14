07b54e988f57d0dee1eef8f109c993d058064652
Lvhacker1 <lavasha2@hotmail.com>
2025-11-14 14:00:50 +0100

    add hover animation to link

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -54,7 +54,7 @@ const LoginPage = () => {
         </form>
         <p className="mt-4 text-center text-sm">
           {loginText.alternativeText}{' '}
-          <Link href={loginText.alternativeLink} className="text-blue-600 hover:underline">
+          <Link href={loginText.alternativeLink} className="inline-block text-blue-600 hover:underline hover:scale-105 transition-transform duration-200">
             {loginText.alternativeLinkText}
           </Link>
         </p>
