c400014a4ebdcedbd24b338ddb26c9d367fae7d4
Lvhacker1 <lavasha2@hotmail.com>
2025-11-18 12:31:05 +0100

    add types

---
+++ lib/types/registerText.ts
@@ -0,0 +1,13 @@
+export interface RegisterPageText {
+  title: string
+  usernameLabel: string
+  emailLabel: string
+  emailPlaceholder:string
+  passwordLabel: string
+  passwordPlaceholder:string
+  submitButton: string
+  loading: string
+  alternativeText: string
+  alternativeLink: string
+  alternativeLinkText: string
+}
