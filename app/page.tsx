8b4166f59d79bada48ecca3711f851cf69a88395
Lvhacker1 <lavasha2@hotmail.com>
2025-11-19 13:57:21 +0100

    setup home page, styling and content to follow

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,20 @@
+import { homePageText } from "@/lib/data/homePage";
+import Link from "next/link";
 
 const HomePage = () => {
   return (
     <div>
-
+      <h1>{homePageText.title}</h1>
+      <div>
+        <Link
+          href={homePageText.loginLink}>
+          {homePageText.loginText}
+        </Link>
+        <Link
+          href={homePageText.registerLink}>
+          {homePageText.registerText}
+        </Link>
+      </div>
     </div>
   );
 }
