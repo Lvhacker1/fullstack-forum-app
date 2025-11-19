98cb2290c3d1a87b68fd62cb2a3d17bc4abfdfed
Lvhacker1 <lavasha2@hotmail.com>
2025-11-19 16:52:44 +0100

    rename homepagetext and move to headertext data file

---
+++ lib/data/headerText.ts
@@ -0,0 +1,11 @@
+import type { HeaderText } from '@/lib/types/headerText'
+import { ROUTES } from '@/lib/constants/routes'
+
+export const headerText: HeaderText = {
+  title: 'Welcome to blabla',
+  loginText: 'Login',
+  loginLink: ROUTES.LOGIN,
+  registerText: 'Register',
+  registerLink: ROUTES.REGISTER,
+  welcomeMessage: 'Welcome',
+}
