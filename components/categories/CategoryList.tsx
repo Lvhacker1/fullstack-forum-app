31e5b67b930adf4a288cb3170004bc29173791a1
Lvhacker1 <lavasha2@hotmail.com>
2025-11-19 16:24:21 +0100

    create categoryList component

---
+++ components/categories/CategoryList.tsx
@@ -0,0 +1,22 @@
+import CategoryCard from './CategoryCard'
+import type { Category } from '@/lib/types/categories'
+import { categoryText } from '@/lib/data/categoryText'
+
+const CategoryList = ({ categories }: { categories: Category[] }) => {
+  if (categories.length === 0) {
+    return (
+      <div>
+        <p>{categoryText.noCategoriesFound}</p>
+      </div>
+    )
+  }
+  return (
+    <div>
+      {categories.map((category) => (
+        <CategoryCard key={category.id} category={category} />
+      ))}
+    </div>
+  )
+}
+
+export default CategoryList
