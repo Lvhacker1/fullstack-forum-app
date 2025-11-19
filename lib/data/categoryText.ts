f66f759cb225080195edf8350e8110b0968330c2
Lvhacker1 <lavasha2@hotmail.com>
2025-11-19 14:43:11 +0100

    add category text data

---
+++ lib/data/categoryText.ts
@@ -0,0 +1,7 @@
+import type { CategoryText } from '@/lib/types/categoryText'
+
+export const categoryText: CategoryText = {
+  noCategoriesFound: 'No categories found',
+  topicSingular: 'topic',
+  topicPlural: 'topics',
+}
