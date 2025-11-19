3fea8a1d44bb9b77dd734e45239728caf29d608f
Lvhacker1 <lavasha2@hotmail.com>
2025-11-19 13:42:43 +0100

    add homePage data

---
+++ lib/data/homePage.ts
@@ -0,0 +1,10 @@
+import { ROUTES } from "../constants/routes";
+import { HomePageText } from "../types/homePage";
+
+export const homePageText: HomePageText = {
+  title: 'Welcome to blabla',
+  loginText: 'Login',
+  loginLink: ROUTES.LOGIN,
+  registerText: 'Register',
+  registerLink: ROUTES.REGISTER,
+}
