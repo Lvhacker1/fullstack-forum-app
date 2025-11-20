64e03592c61eaa8afbbc512020b1a563b9091900
Lvhacker1 <lavasha2@hotmail.com>
2025-11-20 16:50:23 +0100

    add data

---
+++ lib/data/createTopicPage.ts
@@ -0,0 +1,9 @@
+import type { CreateTopicPageText } from '../types/createTopicPage.js'
+
+export const createTopicPageText: CreateTopicPageText = {
+    heading: 'Create New Topic',
+    titleLabel: 'Title',
+    contentLabel: 'Content',
+    submitButton: 'Create Topic',
+    loading: 'Creating.....',
+}
