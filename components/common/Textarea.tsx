9b44d59740d4dc34396fc28f4a427d3288779958
Lvhacker1 <lavasha2@hotmail.com>
2025-11-20 14:40:46 +0100

    style textarea

--- components/common/Textarea.tsx
+++ components/common/Textarea.tsx
@@ -5,7 +5,7 @@ type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>
 const Textarea = ({ className = '', ...props }: TextareaProps) => {
   return (
     <textarea
-      className={`p-2 ${className}`}
+      className={`p-2 border rounded-md w-full  border-gray-400 bg-gray-50 focus:outline-none focus:border-black focus:bg-white transition placeholder-gray-400 ${className}`}
       {...props}
     />
   )
