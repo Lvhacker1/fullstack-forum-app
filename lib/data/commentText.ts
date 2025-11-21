40a6af9a8bacdf82f603f9bd7f83447e5e2d34a5
Lvhacker1 <lavasha2@hotmail.com>
2025-11-21 14:49:56 +0100

    add type and data

---
+++ lib/data/commentText.ts
@@ -0,0 +1,6 @@
+import type { CommentText } from '@/lib/types/commentText'
+
+export const commentText: CommentText = {
+    noCommentsYet: 'No comments yet',
+    commentsHeading: 'Comments',
+}
---
+++ lib/types/commentText.ts
@@ -0,0 +1,4 @@
+export interface CommentText {
+    noCommentsYet: string
+    commentsHeading: string
+}
