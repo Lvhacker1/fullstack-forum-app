a9c58a69395c04790dacaca40afd7286530c3331
Lvhacker1 <lavasha2@hotmail.com>
2025-11-21 14:43:02 +0100

    add styling and display username, date, and content

--- components/comments/Comment.tsx
+++ components/comments/Comment.tsx
@@ -2,13 +2,13 @@ import { CommentWithDetails } from "@/lib/types/comments"
 
 const Comment = ({comment}: { comment: CommentWithDetails} ) => {
     return (
-        <div>
-            <div>
-                <span></span>
-                <span></span>
-                <span></span>
+        <div className="border-l-2 border-gray-400 pl-4 py-3">
+            <div className="flex items-center gap-2 text-sm text-black mb-2">
+                <span className="font-semibold">{comment.profiles.username}</span>
+                <span>•</span>
+                <span>{new Date(comment.created_at).toLocaleDateString()}</span>
             </div>
-            <p></p>
+            <p className="text-gray-800 whitespace-pre-wrap">{comment.content}</p>
         </div>
 
     )
