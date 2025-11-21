78ad31df77295478a6eb901f6e89c522961a1ea6
Lvhacker1 <lavasha2@hotmail.com>
2025-11-21 14:36:55 +0100

    add comment component structure

---
+++ components/comments/Comment.tsx
@@ -0,0 +1,17 @@
+import { CommentWithDetails } from "@/lib/types/comments"
+
+const Comment = ({comment}: { comment: CommentWithDetails} ) => {
+    return (
+        <div>
+            <div>
+                <span></span>
+                <span></span>
+                <span></span>
+            </div>
+            <p></p>
+        </div>
+
+    )
+}
+
+export default Comment
