cf185bceaa8cb4d08a91f19a7aacf294a57f2507
Lvhacker1 <lavasha2@hotmail.com>
2025-11-21 14:55:17 +0100

    create commentLIst component to render comments or show 'no comments yet'  message

---
+++ components/comments/CommentList.tsx
@@ -0,0 +1,24 @@
+import Comment from './Comment'
+import type { CommentWithDetails } from '@/lib/types/comments'
+import { commentText } from '@/lib/data/commentText'
+
+const CommentList = ({ comments}: { comments: CommentWithDetails[] }) => {
+    if (comments.length === 0) {
+        return (
+            <div className="text-center py-8 text-gray-800">
+                {commentText.noCommentsYet}
+            </div>
+        )
+  }
+
+
+  return (
+    <div className="space-y-4 text-center">
+        {comments.map((comment) => (
+            <Comment key={comment.id} comment={comment} />
+        ))}
+    </div>
+  )
+}
+
+export default CommentList
