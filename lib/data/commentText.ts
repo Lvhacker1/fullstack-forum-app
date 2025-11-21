9a7357941bca582104d51beef51b7681e4f75a0c
Lvhacker1 <lavasha2@hotmail.com>
2025-11-21 15:55:04 +0100

    add comment form data

--- lib/data/commentText.ts
+++ lib/data/commentText.ts
@@ -3,4 +3,7 @@ import type { CommentText } from '@/lib/types/commentText'
 export const commentText: CommentText = {
     noCommentsYet: 'No comments yet',
     commentsHeading: 'Comments',
+    commentPlaceholder: 'Write a comment...',
+    submitButton: 'Post Comment',
+    loginToComment: 'Login to comment',
 }
