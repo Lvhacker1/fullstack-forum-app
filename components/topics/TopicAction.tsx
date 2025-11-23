f482d73bee2873726902862a7abed1d33780008d
Lvhacker1 <lavasha2@hotmail.com>
2025-11-23 13:44:14 +0100

    add delete functionality to topicActions component

--- components/topics/TopicAction.tsx
+++ components/topics/TopicAction.tsx
@@ -2,6 +2,7 @@
 import Link from 'next/link'
 import Button from '@/components/common/Button'
 import { topicActionsText } from '@/lib/data/topicPage'
+import deleteTopic from '@/lib/actions/topics/deleteTopic'
 
 interface TopicActionsProps {
     topicId: string
@@ -9,7 +10,16 @@ interface TopicActionsProps {
     topicSlug: string
 }
 
-const TopicActions = ({categorySlug, topicSlug}: TopicActionsProps) => {
+const TopicActions = ({categorySlug, topicSlug, topicId}: TopicActionsProps) => {
+    const handleDelete = async () => {
+        if (!confirm(topicActionsText.deleteConfirm)) 
+            return
+
+        const result = await deleteTopic(topicId, categorySlug)
+        if (result?.error) {
+            alert(`Error deleting topic: ${result.error}`)
+        }
+    }
 
   return (
     <div className="flex gap-2">
@@ -18,7 +28,7 @@ const TopicActions = ({categorySlug, topicSlug}: TopicActionsProps) => {
             {topicActionsText.editButton}
         </Button>
         </Link>
-        <Button variant="danger">
+        <Button variant="danger" onClick={handleDelete}>
             {topicActionsText.deleteButton}
         </Button>
     </div>
