fa5cd67cd43129aaf35edb032a12b6b0f510cd6c
Lvhacker1 <lavasha2@hotmail.com>
2025-11-23 14:17:01 +0100

    add data

---
+++ lib/data/categoryPage.ts
@@ -0,0 +1,8 @@
+import { CategoryPage } from "../types/categoryPage";
+
+
+export const categoryPage: CategoryPage = {
+    newTopicButton: "New Topic",
+    noTopicsMessage: "No topics yet",
+    byText: "by",
+}
