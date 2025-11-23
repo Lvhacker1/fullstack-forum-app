7f59fa3c5938a253dca6b4a560eed6ef40884f20
Lvhacker1 <lavasha2@hotmail.com>
2025-11-23 13:23:54 +0100

    add data

--- lib/data/topicPage.ts
+++ lib/data/topicPage.ts
@@ -1,5 +1,11 @@
-import { TopicPageText } from "../types/topicPage";
+import { TopicActionsText, TopicPageText } from "../types/topicPage";
 
 export const topicPageText: TopicPageText = {
     byText: "By"
+}
+
+export const topicActionsText: TopicActionsText = {
+    editButton: 'Edit',
+    deleteButton: 'Delete',
+    deleteConfirm: 'Are u sure you want to delete this topic?',
 }
