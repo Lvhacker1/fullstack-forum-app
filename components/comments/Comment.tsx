e82fcb2431659afd3a1acb671ed7475fc3cbc2b3
Lvhacker1 <lavasha2@hotmail.com>
2025-11-25 14:46:04 +0100

    add styling

--- components/comments/Comment.tsx
+++ components/comments/Comment.tsx
@@ -12,17 +12,17 @@ const Comment = ({ comment, currentUserId, categorySlug, topicSlug }: CommentPro
     const isOwner = currentUserId && currentUserId === comment.user_id
     return (
         <div className="border-l-2 border-gray-400 pl-4 py-3">
-            <div>
-            <div className="flex items-center gap-2 text-sm text-black mb-2">
-                <span className="font-semibold">{comment.profiles.username}</span>
-                <span>•</span>
-                <span>{new Date(comment.created_at).toLocaleDateString()}</span>
-            </div>
-            <div>
-                {isOwner && (
-                    <CommentActions commentId={comment.id} categorySlug={categorySlug} topicSlug={topicSlug}/>
-                )}
-            </div>
+            <div className="flex items-center justify-between mb-2">
+                <div className="flex items-center gap-2 text-sm text-black mb-2">
+                    <span className="font-semibold">{comment.profiles.username}</span>
+                    <span>•</span>
+                    <span>{new Date(comment.created_at).toLocaleDateString()}</span>
+                </div>
+                <div>
+                    {isOwner && (
+                        <CommentActions commentId={comment.id} categorySlug={categorySlug} topicSlug={topicSlug}/>
+                    )}
+                </div>
             </div>
             <p className="text-gray-800 whitespace-pre-wrap">{comment.content}</p>
         </div>
