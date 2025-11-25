25e9c464ee99bb8c6eb727efbbefd848cfcc61d8
Lvhacker1 <lavasha2@hotmail.com>
2025-11-25 12:06:46 +0100

    uppdate data

--- lib/data/editTopicPage.ts
+++ lib/data/editTopicPage.ts
@@ -1,11 +1,10 @@
 import { EditTopicPageText } from "../types/editTopicPage";
 
-
 export const editTopicPageText: EditTopicPageText = {
     heading: "Edit Topic",
     titleLabel: "Title",
     contentLabel: "Content",
-    submitButton: "Update Topic",
-    loading: "Updating...",
+    submitButton: "Save Changes",
+    loading: "Saving...",
     cancelButton: "Cancel",
 }
