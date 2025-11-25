d2c474b28dcdbbbc8252d509f0e006fb03eb40fa
Lvhacker1 <lavasha2@hotmail.com>
2025-11-25 12:29:47 +0100

    add state and fetch logic for loading topic data

--- app/category/[slug]/[topicSlug]/edit/page.tsx
+++ app/category/[slug]/[topicSlug]/edit/page.tsx
@@ -1,3 +1,5 @@
+import { notFound } from "next/navigation";
+import { use, useEffect, useState } from "react";
 
 interface EditTopicPageProps {
     params: Promise<{
@@ -6,7 +8,33 @@ interface EditTopicPageProps {
     }>
 }
 
-const EditTopicPage = () => {
+const EditTopicPage = ({params}: EditTopicPageProps) => {
+    const {slug, topicSlug} = use(params)
+    const [title, setTitle] = useState('')
+    const [content, setContent] = useState('')
+    const [loading, setLoading] = useState(true)
+    const [error, setError] = useState('')
+    const [topicId, setTopicId] = useState('')
+
+    useEffect (() => {
+        const fetchTopic = async () => {
+            try {
+                const response = await fetch(`/api/category/${slug}/${topicSlug}`)
+                if (!response.ok) return notFound()
+
+                    const data = await response.json()
+                    setTopicId(data.id)
+                    setTitle(data.title)
+                    setContent(data.content)
+                    setLoading(false)
+            } catch {
+                setError('Failed to load topic')
+                setLoading(false)
+            }
+        }
+        fetchTopic()
+    }, [slug, topicSlug])
+
     return (
         <div>
 
