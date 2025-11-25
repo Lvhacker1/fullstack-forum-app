78257eee4ad10165e7352e23a23ac4303e400e96
Lvhacker1 <lavasha2@hotmail.com>
2025-11-25 12:06:23 +0100

    add data

---
+++ lib/data/editTopicPage.ts
@@ -0,0 +1,11 @@
+import { EditTopicPageText } from "../types/editTopicPage";
+
+
+export const editTopicPageText: EditTopicPageText = {
+    heading: "Edit Topic",
+    titleLabel: "Title",
+    contentLabel: "Content",
+    submitButton: "Update Topic",
+    loading: "Updating...",
+    cancelButton: "Cancel",
+}
