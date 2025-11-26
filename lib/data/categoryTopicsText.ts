fb4fdd02f83ca5c1fe79fcf425393c0044bec263
Lvhacker1 <lavasha2@hotmail.com>
2025-11-26 14:55:31 +0100

    add data

---
+++ lib/data/categoryTopicsText.ts
@@ -0,0 +1,9 @@
+import type { CategoryTopicsText } from '@/lib/types/categoryTopicsText'
+
+export const categoryTopicsText: CategoryTopicsText = {
+    newTopicButton: 'New Topic',
+    noTopicsMessage: 'No topics yet',
+    byText: 'By',
+    searchResultsFor: 'Results for',
+    noSearchResults: 'No topics found',
+}
