58b10e2f498cecd92c0c7b48140a844539f3f68e
Lvhacker1 <lavasha2@hotmail.com>
2025-11-26 13:34:21 +0100

    add data

---
+++ lib/data/searchPage.ts
@@ -0,0 +1,9 @@
+import type { SearchPageText } from '@/lib/types/searchPage'
+
+export const searchPageText: SearchPageText = {
+    heading: 'Search Topics',
+    placeholder: 'Search by title...',
+    searchButton: 'Search',
+    noResults: 'No topics found',
+    resultsFor: 'Results for',
+}
