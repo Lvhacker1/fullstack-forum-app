b5e8c6b46570afdc8cf42207b9ef8725474cad45
Lvhacker1 <lavasha2@hotmail.com>
2025-11-30 16:58:47 +0100

    redesign Input component with error state

--- components/common/Input.tsx
+++ components/common/Input.tsx
@@ -1,11 +1,21 @@
 import { InputHTMLAttributes } from 'react'
 
-type InputProps = InputHTMLAttributes<HTMLInputElement>
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {error?: boolean}
 
-const Input = ({ className = '', ...props }: InputProps) => {
+const Input = ({ className = '', error = false, ...props }: InputProps) => {
   return (
     <input
-      className={`px-3 py-2  w-full border rounded-md ${className}`}
+      className={`
+        w-full px-3 py-2 rounded-lg border bg-white text-gray-900 placeholder:text-gray-400
+        transition-all duration-200
+        focus:outline-none focus:ring-2 focus:ring-offset-1
+        disabled:opacity-50 disabled:bg-gray-50
+        ${error 
+          ? 'border-red-500 focus:border-red-500 focus:ring-red-200' 
+          : 'border-gray-300 focus:border-blue-600 focus:ring-blue-100'
+        }
+        ${className}
+      `}
       {...props}
     />
   )
