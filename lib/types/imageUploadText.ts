e4bb8ffc8729a2b164c8f5a21903814463b3c92e
Lvhacker1 <lavasha2@hotmail.com>
2025-12-02 14:11:15 +0100

    add types

--- lib/types/imageUploadText.ts
+++ lib/types/imageUploadText.ts
@@ -2,4 +2,11 @@ export interface ImageUploadText {
     label: string
     uploading: string
     removeButton: string
+    clickToSelect: string
+    fileTypes: string
+    cropTitle: string
+    zoomLabel: string
+    cancelButton: string
+    saveButton: string
+    uploadingButton: string
 }
