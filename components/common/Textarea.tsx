21477bc22554e45e4a1303b9de93f8e4da3fceb2
Lvhacker1 <lavasha2@hotmail.com>
2025-12-02 14:57:24 +0100

    switch to interface, add error prop, and restyle Textarea component

--- components/common/Textarea.tsx
+++ components/common/Textarea.tsx
@@ -1,21 +1,23 @@
 import { TextareaHTMLAttributes } from 'react'
 
-type TextareaProps = TextareaHTMLAttributes<HTMLTextAreaElement>
+interface TextareaProps extends TextareaHTMLAttributes<HTMLTextAreaElement> {
+  error?: boolean
+}
 
-const Textarea = ({ className = '', ...props }: TextareaProps) => {
+const Textarea = ({ className = '', error = false, ...props }: TextareaProps) => {
   return (
     <textarea
       className={`
-        p-2 
-        border 
-        rounded-md w-full 
-        border-gray-400 
-        bg-gray-50 
-        focus:outline-none 
-        focus:border-black 
-        focus:bg-white 
-        transition 
-        placeholder-gray-400 
+        px-4 py-3 rounded-lg bg-transparent border w-full 
+        text-slate-200 placeholder:text-slate-600
+        transition-all duration-200
+        focus:outline-none focus:ring-1 focus:ring-offset-0
+        disabled:opacity-50 disabled:cursor-not-allowed
+        resize-y min-h-[150px]
+        ${error 
+          ? 'border-red-900 focus:border-red-500 focus:ring-red-500' 
+          : 'border-slate-800 hover:border-slate-600 focus:border-blue-500 focus:ring-blue-500'
+        }
         ${className}`}
       {...props}
     />
