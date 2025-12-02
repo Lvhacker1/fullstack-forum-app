aff293392ae9ef14cccae451842130b6923af9a7
Lvhacker1 <lavasha2@hotmail.com>
2025-12-02 15:35:30 +0100

    restyle CommentList component

--- components/comments/CommentList.tsx
+++ components/comments/CommentList.tsx
@@ -14,14 +14,16 @@ interface CommentListProps {
 const CommentList = ({ comments, currentUserId, categorySlug, topicSlug, topicOwnerId, topicId }: CommentListProps) => {
     if (comments.length === 0) {
         return (
-            <div className="text-center py-8 text-gray-800">
-                {commentListText.noCommentsYet}
+            <div className="text-center py-12 border border-dashed border-slate-800 rounded-xl">
+                <p className="text-slate-500 font-medium">
+                    {commentListText.noCommentsYet}
+                </p>
             </div>
         )
   }
 
   return (
-    <div className="space-y-4 text-center">
+    <div className="space-y-6">
         {comments.map((comment) => (
             <Comment
             key={comment.id} 
